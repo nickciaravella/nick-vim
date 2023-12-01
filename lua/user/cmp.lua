2aa0911739146fcd63d87ba97926c9fa125cf9d9
Nick Ciaravella <nickciaravella@Nicks-MBP.lan>
2023-11-30 22:29:27 -0800

    Autocompletion formatting update

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -94,21 +94,14 @@ cmp.setup {
       "s",
     }),
   },
-  formatting = {
-    fields = { "kind", "abbr", "menu" },
-    format = function(entry, vim_item)
-      -- Kind icons
-      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-      vim_item.menu = ({
-        nvim_lsp = "[LSP]",
-        luasnip = "[Snippet]",
-        buffer = "[Buffer]",
-        path = "[Path]",
-      })[entry.source.name]
-      return vim_item
-    end,
-  },
+  -- formatting = {
+  --   fields = { "kind", "abbr", "menu" },
+  --   format = function(entry, vim_item)
+  --     vim_item.menu = vim_item.kind
+  --     vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+  --     return vim_item
+  --   end,
+  -- },
   sources = {
     -- The order here is the order that autocompletions show up.
     { name = "nvim_lsp" },
