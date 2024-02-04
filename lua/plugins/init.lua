2a611ab5d38abbf05e705a673103350632a7a897
Nick Ciaravella <nick.ciaravella@live.com>
2024-02-03 21:50:22 -0800

    Replacing null-ls with none-ls

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,11 +13,5 @@ return {
         event = "VeryLazy",
         config = function() require('user/lsp/mason-lspconfig') end
     },
-    {
-        "jose-elias-alvarez/null-ls.nvim",                                      -- for formatters and linters
-        dependencies = { "nvim-lua/plenary.nvim" },
-        event = "VeryLazy",
-        config = function() require('user/null-ls') end
-    },
 }
 
