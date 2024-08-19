e589bb429c0b244f5909699f17383bda09543b50
Nick Ciaravella <nick.ciaravella@live.com>
2024-08-19 15:49:03 -0700

    +ts-autag, -better-ts-errors

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,21 +1,19 @@
--- Highlight what is being copied during a yank
-vim.cmd [[
-  augroup _general_settings
-    autocmd!
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR>
-    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200})
-    autocmd BufWinEnter * :set formatoptions-=cro
-    autocmd FileType qf set nobuflisted
-  augroup end
-]]
-
 -- Trim trailing whitespace when saving a file.
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    pattern = {"*"},
-    callback = function()
-      local save_cursor = vim.fn.getpos(".")
-      pcall(function() vim.cmd [[%s/\s\+$//e]] end)
-      vim.fn.setpos(".", save_cursor)
-    end,
+	pattern = { "*" },
+	callback = function()
+		local save_cursor = vim.fn.getpos(".")
+		pcall(function()
+			vim.cmd([[%s/\s\+$//e]])
+		end)
+		vim.fn.setpos(".", save_cursor)
+	end,
 })
 
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	desc = "Highlight when yanking text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+	end,
+})
