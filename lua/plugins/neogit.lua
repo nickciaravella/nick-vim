52631cbea6051b945cb5c746177e158cf2d581db
Nick Ciaravella <nick.ciaravella@live.com>
2024-09-09 12:39:29 -0700

    neogit quick commit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -12,18 +12,18 @@ return {
 			kind = "vsplit",
 			commit_editor = { kind = "floating" },
 		})
+
 		vim.keymap.set("n", "<leader>go", "<CMD>Neogit<CR>")
 		vim.keymap.set("n", "<leader>gc", neogit.action("commit", "commit"))
-
-		vim.keymap.set("n", "<leader>gq", function()
-			local commitMessage = vim.fn.input("Commit message")
-			neogit.action("commit", "commit", { "-m " .. commitMessage })
-		end)
-
-		vim.keymap.set("n", "<leader>gC", neogit.action("commit", "commit", { "--verbose", "--all" }))
+		vim.keymap.set("n", "<leader>gC", neogit.action("commit", "commit", { "--verbose", "-all" }))
 		vim.keymap.set("n", "<leader>gp", neogit.action("pull", "from_pushremote"))
 		vim.keymap.set("n", "<leader>gP", neogit.action("push", "to_pushremote"))
 		vim.keymap.set("n", "<leader>gz", neogit.action("stash", "pop"))
 		vim.keymap.set("n", "<leader>gZ", neogit.action("stash", "push"))
+
+		vim.keymap.set("n", "<leader>gq", function()
+			local commitMessage = vim.fn.input("Commit message")
+			neogit.action("commit", "commit", { "--all", "-m " .. commitMessage })()
+		end)
 	end,
 }
